4c1652415703e15c34ea4b5827dd7af6ddd27bba
Craig Gidney <craig.gidney@gmail.com>
2017-11-12 02:09:56 -0800

    StabilizerCircuitState

---
+++ src/sim/StabilizerCircuitState.js
@@ -0,0 +1,142 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {DetailedError} from 'src/base/DetailedError.js'
+import {ObservableProduct} from 'src/sim/ObservableProduct.js'
+import {MeasurementResult} from 'src/sim/MeasurementResult.js'
+import {StabilizerQubit} from 'src/sim/StabilizerQubit.js'
+
+/**
+ * The state of all the qubits in a stabilizer circuit, with operations to mutate the state.
+ */
+class StabilizerCircuitState {
+    /**
+     * Initializes a new empty circuit state.
+     */
+    constructor() {
+        this.qubit_map = new Map();
+        this._id_counter = 0;
+    }
+
+    /**
+     * @returns {!int}
+     * @private
+     */
+    _next_id() {
+        return this._id_counter++;
+    }
+
+    /**
+     * Adds a new qubit, initialized into the off state, to the circuit.
+     * @returns {!int} The qubit's handle, used when specifying operation targets.
+     */
+    add_off_qubit() {
+        let id = this._next_id();
+        this.qubit_map.set(id, new StabilizerQubit(
+            new ObservableProduct(new Set(['q' + id])),
+            new ObservableProduct()));
+        return id;
+    }
+
+    /**
+     * Adds a new qubit, in an unknown state, to the circuit.
+     * @returns {!int} The qubit's handle, used when specifying operation targets.
+     */
+    add_unknown_qubit() {
+        let id = this._next_id();
+        this.qubit_map.set(id, new StabilizerQubit(
+            new ObservableProduct(new Set(['x' + id])),
+            new ObservableProduct(new Set(['z' + id]))));
+        return id;
+    }
+
+    /**
+     * Discards a qubit from the circuit.
+     * @param {!int} index The handle of the qubit to drop.
+     */
+    drop_qubit(index) {
+        this.measure(index);
+        this.qubit_map.delete(index);
+    }
+
+    /**
+     * @param {!int} index
+     * @param {!function(!StabilizerQubit): !StabilizerQubit} action
+     * @private
+     */
+    _apply(index, action) {
+        let q = this.qubit_map.get(index);
+        if (q === undefined) {
+            throw new DetailedError('Bad qubit index.', {index});
+        }
+        this.qubit_map.set(index, action(q));
+    }
+
+    /**
+     * @param {!int} index1
+     * @param {!int} index2
+     * @param {!function(!StabilizerQubit, !StabilizerQubit): !Array.<!StabilizerQubit>} action
+     * @private
+     */
+    _apply2(index1, index2, action) {
+        let a = this.qubit_map.get(index1);
+        let b = this.qubit_map.get(index2);
+        if (a === undefined || b === undefined) {
+            throw new DetailedError('Bad qubit index.', {index1, index2});
+        }
+        let [c, d] = action(a, b);
+        this.qubit_map.set(index1, c);
+        this.qubit_map.set(index2, d);
+    }
+
+    x(index) {
+        this._apply(index, e => e.sqrtX().sqrtX());
+    }
+
+    s(index) {
+        this._apply(index, e => e.sqrtZ());
+    }
+
+    z(index) {
+        this._apply(index, e => e.sqrtZ().sqrtZ());
+    }
+
+    h(index) {
+        this._apply(index, e => e.sqrtY().sqrtX().sqrtX());
+    }
+
+    cz(index1, index2) {
+        this._apply2(index1, index2, (a, b) => a.cz(b));
+    }
+
+    measure(index, reset=false) {
+        let q = this.qubit_map.get(index);
+        if (q === undefined) {
+            throw new DetailedError('Bad qubit index.', {index});
+        }
+
+        let {new_state: s, result: m} = q.measureZ('m' + this._next_id());
+        this.qubit_map.set(index, s);
+        for (let k of [...this.qubit_map.keys()]) {
+            let p = this.qubit_map.get(k);
+            this.qubit_map.set(k, p.rewriteWithMeasurementResult(m));
+        }
+        if (reset && m.result) {
+            this.x(index);
+        }
+        return m.result === -1;
+    }
+}
+
+export {StabilizerCircuitState};
---
+++ test/sim/StabilizerCircuit.test.js
@@ -0,0 +1,63 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {Suite, assertThat, assertThrows, assertTrue, assertFalse} from 'test/TestUtil.js'
+import {MeasurementResult} from 'src/sim/MeasurementResult.js'
+import {ObservableProduct} from 'src/sim/ObservableProduct.js'
+import {StabilizerQubit} from 'src/sim/StabilizerQubit.js'
+import {StabilizerCircuitState} from 'src/sim/StabilizerCircuitState.js'
+
+let suite = new Suite('StabilizerCircuitState');
+
+suite.test('teleport', () => {
+    let c = new StabilizerCircuitState();
+
+    // Pre-shared entanglement.
+    let a = c.add_off_qubit();
+    let b = c.add_off_qubit();
+    c.h(a);
+    c.h(b);
+    c.cz(a, b);
+    c.h(b);
+
+    // Entangled message.
+    let e = c.add_off_qubit();
+    let m = c.add_off_qubit();
+    c.h(e);
+    c.h(m);
+    c.cz(e, m);
+    c.h(m);
+
+    // Teleport message.
+    c.h(a);
+    c.cz(m, a);
+    c.h(a);
+    c.h(m);
+    if (c.measure(a)) {
+        c.x(b);
+    }
+    if (c.measure(m)) {
+        c.z(b);
+    }
+
+    // Unmake entanglement between b and e.
+    c.h(b);
+    c.cz(e, b);
+    c.h(b);
+    c.h(e);
+
+    // They should be back into the off state.
+    assertFalse(c.measure(b));
+    assertFalse(c.measure(e));
+});
