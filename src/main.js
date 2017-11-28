b44a106e6209ed00463c072ca433d22e31cbf2c3
Craig Gidney <craig.gidney@gmail.com>
2017-11-28 01:13:52 -0800

    Hacking miscellaneous functionality together - Matching errors to fixes - Drawing error/fix cycles - Refining cycles away - Drawing border types

--- src/main.js
+++ src/main.js
@@ -4,56 +4,183 @@ import {MeasurementResult} from 'src/sim/MeasurementResult.js'
 import {StabilizerQubit} from 'src/sim/StabilizerQubit.js'
 import {StabilizerCircuitState} from 'src/sim/StabilizerCircuitState.js'
 import {SurfaceCode} from 'src/sim/SurfaceCode.js'
+import {describe} from "src/base/Describe.js";
+
+window.onerror = function(msg, url, line, col, error) {
+    document.getElementById('err_msg').textContent = describe(msg);
+    document.getElementById('err_line').textContent = describe(line);
+    document.getElementById('err_time').textContent = '' + new Date().getMilliseconds();
+    if (error instanceof DetailedError) {
+        document.getElementById('err_gen').textContent = describe(error.details);
+    }
+    console.warn(msg, url, line, col, error);
+};
 
 let canvas = /** @type {!HTMLCanvasElement} */ document.getElementById('main-canvas');
 
 let surface = new SurfaceCode(50, 30);
+surface.cycle();
+surface.zero();
+surface.cycle();
+surface.clearFlips();
 
 let diam = 20;
-canvas.width = diam * surface.width;
-canvas.height = diam * surface.height;
+let canvas_padding = 10;
+canvas.width = diam * surface.width + canvas_padding*2;
+canvas.height = diam * surface.height + canvas_padding*2;
 
 function draw() {
     let ctx = canvas.getContext('2d');
+    ctx.save();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.translate(canvas_padding, canvas_padding);
     for (let i = 0; i < surface.width; i++) {
         for (let j = 0; j < surface.height; j++) {
-            let x = i*diam;
-            let y = j*diam;
-            if ((i & 1) !== (j & 1)) {
-                ctx.fillStyle = '#FFF';
-            } else if ((i & 1) === 0) {
+
+            let x = i * diam;
+            let y = j * diam;
+            ctx.fillStyle = '#888';
+            if (surface.isDataQubit(i, j)) {
+                ctx.fillStyle = '#FFF'; // data qubit
+            } else if (surface.isZCheckQubit(i, j)) {
                 if (surface.last_result[i][j] === true) {
-                    ctx.fillStyle = '#A62';
+                    ctx.fillStyle = '#080'; // Z fail
                 } else if (surface.last_result[i][j] === false) {
-                    ctx.fillStyle = '#DFD';
+                    ctx.fillStyle = '#DFD'; // Z normal
+                }
+            } else if (surface.isXCheckQubit(i, j)) {
+                if (surface.last_result[i][j] === true) {
+                    ctx.fillStyle = '#008'; // X fail
+                } else if (surface.last_result[i][j] === false) {
+                    ctx.fillStyle = '#DDF'; // X normal
                 }
             } else {
-                if (surface.last_result[i][j] === true) {
-                    ctx.fillStyle = '#F6F';
-                } else if (surface.last_result[i][j] === false) {
-                    ctx.fillStyle = '#DDF';
+                if (surface.last_result[i][j]) {
+                    ctx.fillStyle = '#000'; // hole
+                } else {
+                    ctx.fillStyle = '#444'; // hole
                 }
             }
             ctx.fillRect(x, y, diam, diam);
         }
     }
+
+    for (let i = -1; i <= surface.width; i++) {
+        for (let j = -1; j <= surface.height; j++) {
+            if (!surface.isHole(i, j)) {
+                continue;
+            }
+
+            let x = i * diam;
+            let y = j * diam;
+
+            let drawBorder = (di, dj) => {
+                let type = surface.borderType(i, j, di, dj);
+                if (type !== undefined) {
+                    ctx.fillStyle = type === 'Z' ? '#0A0' : '#00A';
+                    let ai = Math.abs(di);
+                    let aj = Math.abs(dj);
+                    ctx.fillRect(
+                        x + (diam-3)*(di+1)/2*ai,
+                        y + (diam-3)*(dj+1)/2*aj,
+                        aj*diam + 3*ai,
+                        ai*diam + 3*aj);
+                }
+            };
+            drawBorder(-1, 0);
+            drawBorder(+1, 0);
+            drawBorder(0, -1);
+            drawBorder(0, +1);
+        }
+    }
+
+    for (let i = 0; i < surface.width; i++) {
+        for (let j = 0; j < surface.height; j++) {
+            let x = i * diam + 0.5;
+            let y = j * diam + 0.5;
+            if (!surface.isDataQubit(i, j)) {
+                continue
+            }
+            if (surface.xFlips[i][j]) {
+                ctx.strokeStyle = '#080';
+                ctx.beginPath();
+                if (surface.isXCheckRow(j)) {
+                    ctx.moveTo(x+diam/2, y - diam/2);
+                    ctx.lineTo(x+diam/2, y + diam*3/2);
+                } else {
+                    ctx.moveTo(x - diam/2, y + diam/2);
+                    ctx.lineTo(x + diam*3/2, y + diam/2);
+                }
+                ctx.stroke();
+            }
+            if (surface.zFlips[i][j]) {
+                ctx.strokeStyle = '#008';
+                ctx.beginPath();
+                if (!surface.isXCheckRow(j)) {
+                    ctx.moveTo(x+diam/2, y - diam/2);
+                    ctx.lineTo(x+diam/2, y + diam*3/2);
+                } else {
+                    ctx.moveTo(x - diam/2, y + diam/2);
+                    ctx.lineTo(x + diam*3/2, y + diam/2);
+                }
+                ctx.stroke();
+            }
+        }
+    }
+    ctx.restore();
 }
 
 setInterval(() => {
-    surface.zero();
     surface.error();
     surface.cycle();
     draw();
 }, 100);
 
+document.onkeydown = ev => {
+    if (ev.keyCode === 32) {
+        ev.preventDefault();
+        surface.correct();
+        surface.cycle();
+        draw();
+    } else if (ev.keyCode === 65) {
+        ev.preventDefault();
+        surface.clean_areas();
+    } else if (ev.keyCode === 69) {
+        ev.preventDefault();
+        surface.error(0.001);
+    }
+};
+
 canvas.onmousedown = ev => {
-    let i = Math.floor(ev.x / diam);
-    let j = Math.floor(ev.y / diam);
-    if (i >= 0 && i < surface.width && j >= 0 && j < surface.height && ((i & 1) !== (j & 1))) {
-        if (ev.button === 0) {
-            surface.state.x(surface.qubits[i][j]);
+    ev.preventDefault();
+    let b = canvas.getBoundingClientRect();
+    let i = Math.floor((ev.x - canvas_padding - b.left) / diam);
+    let j = Math.floor((ev.y - canvas_padding - b.top) / diam);
+    if (i >= 0 && i < surface.width && j >= 0 && j < surface.height) {
+        if (ev.ctrlKey) {
+            if (ev.button === 0) {
+                surface.holes[i][j] ^= true;
+                if (surface.holes[i][j]) {
+                    surface.measure(i, j);
+                    surface.xFlips[i][j] = false;
+                    surface.zFlips[i][j] = false;
+                }
+
+            }
         } else {
-            surface.state.z(surface.qubits[i][j]);
+            if (surface.isDataQubit(i, j)) {
+                if (ev.button === 0) {
+                    surface.state.x(surface.qubits[i][j]);
+                    if (!ev.altKey) {
+                        surface.xFlips[i][j] ^= true;
+                    }
+                } else {
+                    surface.state.z(surface.qubits[i][j]);
+                    if (!ev.altKey) {
+                        surface.zFlips[i][j] ^= true;
+                    }
+                }
+            }
         }
         surface.cycle();
         draw();
--- src/sim/SurfaceCode.js
+++ src/sim/SurfaceCode.js
@@ -5,29 +5,53 @@ import {StabilizerQubit} from 'src/sim/StabilizerQubit.js'
 import {StabilizerCircuitState} from 'src/sim/StabilizerCircuitState.js'
 
 
+function makeGrid(width, height, generatorFunc) {
+    let grid = [];
+    for (let i = 0; i < width; i++) {
+        let row = [];
+        for (let j = 0; j < height; j++) {
+            row.push(generatorFunc(i, j));
+        }
+        grid.push(row);
+    }
+    return grid;
+}
+
 class SurfaceCode {
     constructor(width, height) {
         this.width = width;
         this.height = height;
         this.state = new StabilizerCircuitState();
-
-        this.qubits = [];
-        this.last_result = [];
+        this.holes = makeGrid(width, height, () => false);
+        this.xFlips = makeGrid(width, height, () => false);
+        this.zFlips = makeGrid(width, height, () => false);
+        this.last_result = makeGrid(width, height, () => undefined);
         this.target = this.state.add_off_qubit();
-        for (let i = 0; i < width; i++) {
-            let r = [];
-            let m = [];
-            for (let j = 0; j < height; j++) {
-                if ((i & 1) !== (j & 1)) {
-                    r.push(this.state.add_off_qubit());
-                } else {
-                    r.push(this.target);
-                }
-                m.push(undefined);
+        this.qubits = makeGrid(width, height,
+            (i, j) => this.isDataQubit(i, j) ? this.state.add_off_qubit(): this.target);
+
+        this.points = [];
+        for (let i = 0; i < this.width; i++) {
+            for (let j = 0; j < this.height; j++) {
+                this.points.push([i, j]);
             }
-            this.qubits.push(r);
-            this.last_result.push(m);
         }
+    }
+
+    isXCheckCol(col) {
+        return (col & 1) === 1;
+    }
+
+    isZCheckCol(col) {
+        return (col & 1) === 0;
+    }
+
+    isXCheckRow(row) {
+        return (row & 1) === 1;
+    }
+
+    isZCheckRow(row) {
+        return (row & 1) === 0;
     }
 
     neighbors(i, j) {
@@ -35,7 +59,7 @@ class SurfaceCode {
         for (let [di, dj] of [[1, 0], [-1, 0], [0, 1], [0, -1]]) {
             let i2 = i + di;
             let j2 = j + dj;
-            if (i2 >= 0 && i2 < this.width && j2 >= 0 && j2 < this.height) {
+            if (i2 >= 0 && i2 < this.width && j2 >= 0 && j2 < this.height && !this.isHole(i2, j2)) {
                 result.push([i2, j2]);
             }
         }
@@ -55,25 +79,73 @@ class SurfaceCode {
         return this.state.measure(t, true);
     }
 
+    measure(i, j) {
+        this.last_result[i][j] = this.state.measure(this.qubits[i][j], true);
+        return this.last_result[i][j];
+    }
+
+    isHole(i, j) {
+        return !this.isInBounds(i, j) || this.holes[i][j];
+    }
+
+    isInBounds(i, j, padding=0) {
+        return i >= -padding && j >= -padding && i < this.width+padding && j < this.height + padding;
+    }
+
+    //noinspection JSMethodCanBeStatic
+    isZCheckQubit(i, j, ignoreHole=false) {
+        return (ignoreHole || !this.isHole(i, j)) && (i & 1) === 0 && (j & 1) === 0;
+    }
+
+    borderType(i, j, di, dj) {
+        let i2 = i + di;
+        let j2 = j + dj;
+        let h1 = this.isHole(i, j);
+        let h2 = this.isHole(i2, j2);
+        if (h1 === h2) {
+            return undefined;
+        }
+        if (h2) {
+            [i, j, i2, j2] = [i2, j2, i, j];
+        }
+        let z1 = this.isZCheckQubit(i, j, true);
+        let x2 = this.isXCheckQubit(i2, j2, true);
+        return z1 || x2 ? 'Z' : 'X';
+    }
+
+    //noinspection JSMethodCanBeStatic
+    isXCheckQubit(i, j, ignoreHole=false) {
+        return (ignoreHole || !this.isHole(i, j)) && (i & 1) === 1 && (j & 1) === 1;
+    }
+
+    //noinspection JSMethodCanBeStatic
+    isDataQubit(i, j) {
+        return !this.isHole(i, j) && (i & 1) !== (j & 1);
+    }
+
     cycle() {
         for (let i = 0; i < this.width; i++) {
             for (let j = 0; j < this.height; j++) {
-                if ((i & 1) === (j & 1)) {
-                    this.last_result[i][j] = this.square_measure(i, j, (i & 1) === 1);
+                if (this.isXCheckQubit(i, j)) {
+                    this.last_result[i][j] = this.square_measure(i, j, true);
+                } else if (this.isZCheckQubit(i, j)) {
+                    this.last_result[i][j] = this.square_measure(i, j, false);
                 }
             }
         }
     }
 
-    error(p = 0.01) {
+    error(p = 0.0001) {
         for (let i = 0; i < this.width; i++) {
             for (let j = 0; j < this.height; j++) {
-                if (Math.random() < p) {
+                if (this.isDataQubit(i, j) && Math.random() < p) {
                     if (Math.random() < 0.5) {
                         this.state.x(this.qubits[i][j]);
+                        this.xFlips[i][j] ^= true;
                     }
                     if (Math.random() < 0.5) {
                         this.state.z(this.qubits[i][j]);
+                        this.zFlips[i][j] ^= true;
                     }
                 }
             }
@@ -101,6 +173,195 @@ class SurfaceCode {
                 }
             }
         }
+    }
+
+    pairs(points) {
+        let pairs = [];
+        let grid = makeGrid(this.width, this.height, () => undefined);
+        let queue = points.map(e => [e, e]);
+
+        let clear = pt => {
+            for (let i = 0; i < this.width; i++) {
+                for (let j = 0; j < this.height; j++) {
+                    if (grid[i][j] === pt) {
+                        grid[i][j] = undefined;
+                    }
+                }
+            }
+            queue = queue.filter(e => e[1] !== pt);
+        };
+
+        while (queue.length > 0) {
+            let [[x, y], src] = queue[0];
+            queue.splice(0, 1);
+
+            if (this.isHole(x, y)) {
+                clear(src);
+                pairs.push([src, [x, y]]);
+                continue;
+            }
+
+            let dst = grid[x][y];
+            if (src === dst) {
+                continue;
+            }
+            if (dst !== undefined) {
+                clear(src);
+                clear(dst);
+                pairs.push([src, dst])
+            } else {
+                grid[x][y] = src;
+                if (!this.isHole(x + 1, y)) {
+                    queue.push([[x + 2, y], src]);
+                }
+                if (!this.isHole(x - 1, y)) {
+                    queue.push([[x - 2, y], src]);
+                }
+                if (!this.isHole(x, y - 1)) {
+                    queue.push([[x, y - 2], src]);
+                }
+                if (!this.isHole(x, y + 1)) {
+                    queue.push([[x, y + 2], src]);
+                }
+            }
+        }
+        return pairs;
+    }
+
+    clean_areas() {
+        for (let b of [false, true]) {
+            let areas = new Map();
+            let areaVals = [];
+            let reps = [];
+            for (let pt of this.points) {
+                let queue = [pt];
+                let area = [];
+                let hitOppositeTypeSide = false;
+                while (queue.length > 0) {
+                    let [i, j] = queue.pop();
+                    if (b ? (i < 0 || j < 0) : (i >= this.width || j >= this.height)) {
+                        hitOppositeTypeSide = true;
+                    }
+                    let k = j * this.width + i;
+                    if (areas.has(k) || this.isHole(i, j) || (b ? this.xFlips[i][j] : this.zFlips[i][j]) || (b ? this.isZCheckQubit(i, j) : this.isXCheckQubit(i, j))) {
+                        continue;
+                    }
+                    areas.set(k, areaVals.length);
+                    area.push([i, j]);
+
+                    queue.push([i + 1, j]);
+                    queue.push([i - 1, j]);
+                    queue.push([i, j - 1]);
+                    queue.push([i, j + 1]);
+                }
+
+                if (hitOppositeTypeSide) {
+                    areaVals.push(Infinity);
+                } else if (area.length > 0) {
+                    areaVals.push(area.length);
+                    let best_i = undefined;
+                    let best_j = undefined;
+                    let best_d = -Infinity;
+                    let best_n = false;
+                    for (let [i, j] of area) {
+                        if (!this.isDataQubit(i, j)) {
+                            let n = !this.neighbors(i, j).every(([i2, j2]) => !(b ? this.xFlips : this.zFlips)[i2][j2]);
+                            let m = Math.max(Infinity,
+                                ...this.neighbors(i, j).
+                                    filter(([i2, j2]) => area.length - 1 !== areas.get(j2*this.width + i2)).
+                                    map(([i2, j2]) => areaVals[areas.get(j2*this.width + i2)]));
+                            if (m <= area.length) {
+                                continue;
+                            }
+                            let d = Math.min(i, j, this.width - i - 1, this.height - j - 1);
+                            if ((n && !best_n) || (n === best_n && d > best_d)) {
+                                best_i = i;
+                                best_j = j;
+                                best_d = d;
+                                best_n = n;
+                            }
+                        }
+                    }
+                    if (best_i !== undefined) {
+                        reps.push([best_i, best_j]);
+                    }
+                }
+            }
+
+            for (let [i, j] of reps) {
+                if (b ? this.isXCheckQubit(i, j) : this.isZCheckQubit(i, j)) {
+                    for (let [di, dj] of [[-1, 0], [+1, 0], [0, -1], [0, +1]]) {
+                        let i2 = i + di;
+                        let j2 = j + dj;
+                        if (this.isDataQubit(i2, j2)) {
+                            if (b) {
+                                this.xFlips[i2][j2] ^= true;
+                            } else {
+                                this.zFlips[i2][j2] ^= true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    chain(x1, y1, x2, y2, isX) {
+        let flip = isX ?
+            (x, y) => {
+                this.state.x(this.qubits[x][y]);
+                this.xFlips[x][y] ^= true;
+            } :
+            (x, y) => {
+                this.state.z(this.qubits[x][y]);
+                this.zFlips[x][y] ^= true;
+            };
+
+        while (x1 < x2) {
+            flip(x1 + 1, y1);
+            x1 += 2;
+        }
+        while (x1 > x2) {
+            flip(x1 - 1, y1);
+            x1 -= 2;
+        }
+        while (y1 < y2) {
+            flip(x1, y1 + 1);
+            y1 += 2;
+        }
+        while (y1 > y2) {
+            flip(x1, y1 - 1);
+            y1 -= 2;
+        }
+    }
+
+    correct() {
+        let points = [];
+        let points2 = [];
+        for (let i = 0; i < this.width; i++) {
+            for (let j = 0; j < this.height; j++) {
+                if (this.last_result[i][j] === true) {
+                    if (this.isZCheckQubit(i, j)) {
+                        points.push([i, j]);
+                    } else if (this.isXCheckQubit(i, j)) {
+                        points2.push([i, j]);
+                    }
+                }
+            }
+        }
+
+        for (let [[x1, y1], [x2, y2]] of this.pairs(points)) {
+            this.chain(x1, y1, x2, y2, true);
+        }
+
+        for (let [[x1, y1], [x2, y2]] of this.pairs(points2)) {
+            this.chain(x1, y1, x2, y2, false);
+        }
+    }
+
+    clearFlips() {
+        this.xFlips = makeGrid(this.width, this.height, () => false);
+        this.zFlips = makeGrid(this.width, this.height, () => false);
     }
 }
 
